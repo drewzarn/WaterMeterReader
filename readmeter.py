33b7361421c08106fe6f618e898ae4ea8b79d6e4
Drew Zarn <dzarn@amovita.net>
2021-06-25 20:26:26 +0100

    Needle center variables Run every 5s Median blur before finding angle Save angle ring on debug image

--- readmeter.py
+++ readmeter.py
@@ -11,9 +11,11 @@ import threading, queue, math
 debug = 2 #0=none, 1=image and debug, 2=all steps
 cropX = 160
 cropY = 160
+needleCenterX = 300
+needleCenterY = 330
 DIFFERENCE_THRESHOLD_PX = 25
-RED_MIN = np.array([160, 5, 60], np.uint8)
-RED_MAX = np.array([180, 80, 160], np.uint8)
+RED_MIN = np.array([0, 0, 0], np.uint8)
+RED_MAX = np.array([50, 255, 255], np.uint8)
 CHECK_RADIUS = 100
 GALLONS_PER_DEGREE = 10 / 360
 
@@ -83,21 +85,21 @@ with PiCamera() as camera:
 	time.sleep(1)
 
 	while True:
-		#Make it run approx 1hz
+		#Make it run approx every 5s
 		if(captureTime is not None):
-			time.sleep(1.0 - (time.time() - captureTime))
+			sleepTime = 5.0 - (time.time() - captureTime)
+			time.sleep(sleepTime if sleepTime > 0 else 0)
 		print('Capturing')
 		captureTime = time.time()
 		captureTimeFriendly = datetime.now().strftime("%Y%m%d-%H%M%S")
-		debug = 2#(2 if int(captureTime)%60 == 0 else 0)
+		debug = (2 if int(captureTime)%20 == 0 else 0)
 
 		#Grab image and crop
 		rawCapture = PiRGBArray(camera)
 		camera.capture(rawCapture, format="bgr")
 		img = rawCapture.array
-		img = img[320 - cropY:320 + cropY, 340 - cropX:340 + cropX]
+		img = img[needleCenterY - cropY:needleCenterY + cropY, needleCenterX - cropX:needleCenterX + cropX]
 		output_image(img, captureTimeFriendly, 'base', 2)
-		exit()
 		
 		#Convert to HSV and get mask
 		hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -105,20 +107,28 @@ with PiCamera() as camera:
 		mask = cv2.inRange(hsv_img, RED_MIN, RED_MAX)
 		output_image(mask, captureTimeFriendly, 'mask', 2)
 
+		#MedianBlur
+		blur_mask = cv2.medianBlur(mask, 3)
+		output_image(blur_mask, captureTimeFriendly, 'blurmask', 2)
+
+		workingImg = blur_mask
+
 		#Find the angle of the first white pixel
+		debugImg = cv2.cvtColor(workingImg, cv2.COLOR_GRAY2RGB)
 		for angle in reversed(sorted(ANGLE_POINTS.keys())):
 			y, x = ANGLE_POINTS[angle]
 			lastY, lastX = ANGLE_POINTS[0 if angle==359 else angle + 1]
-			if mask[y, x] == 255 and mask[lastY, lastX] == 0:
+			debugImg[y, x] = (0, 255, 0) if workingImg[y, x] == 255 else (0, 0, 255)
+			if workingImg[y, x] == 255 and workingImg[lastY, lastX] == 0:
 				angleCurrent = angle
 				break
 
 		if debug == 2:
-			cv2.putText(mask, str(angleCurrent), (120, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2)
-			output_image(mask, captureTimeFriendly, 'debug', 2)
+			cv2.putText(debugImg, str(angleCurrent), (120, 160), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2)
+			output_image(debugImg, captureTimeFriendly, 'debug', 2)
 
 		if anglePrevious is None or anglePrevious2 is None or anglePrevious3 is None:
-			print('Populating previous angles')
+			print('Populating previous angles', anglePrevious, anglePrevious2, anglePrevious3)
 			anglePrevious3 = anglePrevious2
 			anglePrevious2 = anglePrevious
 			anglePrevious = angleCurrent
@@ -147,7 +157,7 @@ with PiCamera() as camera:
 			QueueMessage(0)
 			continue
 
-		if abs(angleDelta) > 270:
+		if abs(angleDelta) > 90:
 			print(datetime.now().strftime("%H:%M:%S"), 'Giant jump', angleCurrent, angleDelta)
 			QueueMessage(0)
 			continue
