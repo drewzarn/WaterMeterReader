c88cdac1ef37610ec123db9a633147de967a1f92
Drew Zarn <dzarn@amovita.net>
2021-06-28 18:27:13 +0100

    Fix service configs and install

--- service-install.sh
+++ service-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s /meterreading/service-config /lib/systemd/system/readmeter.service
+cp /meterreading/service-config /etc/systemd/system/readmeter.service
 systemctl daemon-reload
 
-systemctl start readmeter.service
 systemctl enable readmeter.service
+service readmeter start
 
-systemctl status readmeter
+service readmeter status
