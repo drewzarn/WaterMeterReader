28678a3a70b68e6ab945b204385cd3d7493d158d
Drew Zarn <dzarn@amovita.net>
2021-09-03 20:27:44 -0600

    Adjsut light to exery 2

--- setup_lights.py
+++ setup_lights.py
@@ -4,4 +4,6 @@ import time
 
 pixels = neopixel.NeoPixel(board.D12, 24)
 for p in range(0, 23):
-    pixels[p] = (120, 120, 0)
+    pixels[p] = (0, 0, 0)
+for p in range(0, 23, 2):
+    pixels[p] = (255, 255, 255)
