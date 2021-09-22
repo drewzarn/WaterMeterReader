f8d0b2e46e32d0c7194ddfa660ccaa3495540be6
Drew Zarn <dzarn@amovita.net>
2021-09-21 18:01:33 -0600

    Make lights a service

--- service-install.sh
+++ service-install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cp /meterreading/service-config /etc/systemd/system/readmeter.service
+cp /meterreading/service-lights-config /etc/systemd/system/meterlights.service
 systemctl daemon-reload
 
 systemctl enable readmeter.service
+systemctl enable meterlights.service
 service readmeter start
+service meterlights start
 
 service readmeter status
+service meterlights status
--- setup_lights.py
+++ setup_lights.py
@@ -2,8 +2,11 @@ import board
 import neopixel
 import time
 
-pixels = neopixel.NeoPixel(board.D12, 24)
-for p in range(0, 23):
-    pixels[p] = (0, 0, 0)
-for p in range(0, 23, 2):
-    pixels[p] = (255, 255, 255)
+while True:
+    print('Setting lights')
+    pixels = neopixel.NeoPixel(board.D12, 24)
+    for p in range(0, 23):
+        pixels[p] = (0, 0, 0)
+    for p in range(0, 23, 2):
+        pixels[p] = (255, 255, 255)
+    time.sleep(30)
